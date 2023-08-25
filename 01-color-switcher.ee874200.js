bb9c18948e75cb28ecd932f51fa5569261bba52e
AlexPazyuk <127767614+AlexPazyuk@users.noreply.github.com>
2023-08-25 20:31:20 +0000

    Deploying to gh-pages from @ AlexPazyuk/goit-js-hw-09@947e0b95d05756738715ee8255909450f14905ba 🚀

---
+++ 01-color-switcher.ee874200.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null,n=function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))};t.addEventListener("click",(function(){d=setInterval(n,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0,document.body.style.backgroundColor=""}))}();
+//# sourceMappingURL=01-color-switcher.ee874200.js.map
